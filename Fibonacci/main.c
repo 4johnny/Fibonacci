d9e49b4c92ba6cc561f67ede8538ed12cfa0443e
4johnny <4johnny@users.noreply.github.com>
2015-01-14 20:20:49 -0800

    Initial impl.

--- Fibonacci/main.c
+++ Fibonacci/main.c
@@ -8,8 +8,71 @@
 
 #include <stdio.h>
 
+int fibonacciIterative(int index) {
+	
+	if (index < 0) return -1;
+	if (index == 0) return 0;
+	
+	// index >= 1
+	int prevPrev;
+	int prev = 0;
+	int fibonacci = 1;
+	for (; index > 1; index--) {
+		
+		prevPrev = prev;
+		prev = fibonacci;
+		
+		fibonacci = prev + prevPrev;
+	}
+
+	return fibonacci;
+}
+
+int fibonacciRecursive(int index) {
+
+	// Base Cases
+	if (index < 0) return -1; // Input validation.
+	if (index == 0) return 0;
+	if (index == 1) return 1;
+	
+	// Recursive Case
+	// index >= 2
+	return fibonacciRecursive(index - 1) + fibonacciRecursive(index - 2);
+}
+
+int recursiveValueFibonacci(int remainingSteps, int previousNumber, int previousPreviousNumber) {
+	
+	if (remainingSteps == 0) return 0;
+	if (remainingSteps == 1) return previousNumber;
+	
+	// remainingSteps >= 2
+	int fibonacci = previousNumber + previousPreviousNumber;
+	if (remainingSteps == 0) return fibonacci;
+	
+	return recursiveValueFibonacci(remainingSteps - 1, fibonacci, previousNumber);
+}
+
+int recursiveReferenceFibonacci(int remainingSteps, int *previousNumber, int *previousPreviousNumber) {
+	if (remainingSteps == 0) return 0;
+	if (remainingSteps == 1) return *previousNumber;
+	
+	// remainingSteps >= 2
+	int fibonacci = *previousNumber + *previousPreviousNumber;
+	if (remainingSteps == 0) return fibonacci;
+	
+	return recursiveReferenceFibonacci(remainingSteps - 1, &fibonacci, previousNumber);
+}
+
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	printf("Hello, World!\n");
-    return 0;
+
+	int prevPrev = 0;
+	int prev = 1;
+	int fibonacciIndex = 20;
+	printf("Fibonacci #%d\n", fibonacciIndex);
+	printf("Iterative: %d\n", fibonacciIterative(fibonacciIndex));
+	printf("Recursive: %d\n", fibonacciRecursive(fibonacciIndex));
+	printf("Recursive By Val: %d\n", recursiveValueFibonacci(fibonacciIndex, 1, 0));
+	printf("Recursive By Ref: %d\n", recursiveReferenceFibonacci(fibonacciIndex, &prev, &prevPrev));
+	
+	return 0;
 }
